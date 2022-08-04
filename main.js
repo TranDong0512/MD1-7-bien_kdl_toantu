26bad75b00ea06fd9124f50aa8e2ddcd9a3cb11b
TranDong0512 <tdong0512@gmail.com>
2022-08-04 20:57:31 +0700

    bai1

---
+++ main.js
@@ -0,0 +1,10 @@
+let a =prompt("Nhập điểm hóa")
+let b =prompt("nhập điểm lý")
+let c=prompt("nhập điểm sinh")
+a = parseFloat(a)
+b = parseFloat(b)
+c = parseFloat(c)
+let d = (a + b + c)/3
+let s = (a + b + c)
+alert("Điểm trung bình bằng: " + d)
+alert("Điểm tổng bằng: " +s)
