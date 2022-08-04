7a65396bfdc8e2178ea26dfc94a5c5737b7eb1a5
TranDong0512 <tdong0512@gmail.com>
2022-08-04 21:43:18 +0700

    dt,cv-hinhtron

---
+++ main3.js
@@ -0,0 +1,7 @@
+let r = prompt("Nhập bán kính r: ")
+r = parseFloat(r)
+let d = r * 2;
+let S = r * r * Math.PI;
+let C = d * 3.14;
+alert("Diện tích hình tròn là:"+S)
+alert("chu vi hình tròn là:"+C)
